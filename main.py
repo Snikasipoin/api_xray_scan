b6d9f45d386a224c45eac38136efaf0e03d38192
Roman Kalmykow <snikasipoin@gmail.com>
2025-05-04 20:22:30 +0600

    Fix 1

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
+import torch
+import torch.nn as nn
+import torchvision.models as models
 
 load_dotenv()
 
@@ -17,6 +20,24 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(RESULT_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load model
+model = models.densenet121(weights=None)
+num_classes = 14
+model.classifier = nn.Linear(model.classifier.in_features, num_classes)
+
+try:
+    checkpoint = torch.load(MODEL_PATH, map_location=device)
+    state_dict = checkpoint.get("state_dict", checkpoint)
+    corrected_state_dict = {k.replace("densenet121.", "").replace("classifier.0.", "classifier."): v for k, v in state_dict.items()}
+    model.load_state_dict(corrected_state_dict)
+except Exception as e:
+    print(f"❌ Ошибка загрузки модели: {e}")
+    exit(1)
+
+model.to(device)
+model.eval()
 
 @app.route("/")
 def hello():
